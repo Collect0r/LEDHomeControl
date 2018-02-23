8234ab07ddeeb1b2b3582e0074c163fba1013f45
Joachim Goebgen <joachim.goebgen@bwedu.de>
2018-02-23 16:43:55 +0100

    minor changes

--- home-api.js
+++ home-api.js
@@ -7,23 +7,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var led = [{
-    mode: 1, R1: 255, G1: 0, B1: 0,
+    R1: 255, G1: 0, B1: 0,
     R2: 0, G2: 200, B2: 0,
     R3: 0, G3: 0, B3: 255,
-    R4: 0, G4: 0, B4: 255
-}];
+    R4: 0, G4: 0, B4: 255 },
+    { mode: 1, rotate: 0, params: 0 },
+    { loop: 50, request: 4 }];
 
 app.use(express['static'](__dirname));
 
+
 // Express route for incoming requests
 app.get('/led', function (req, res) {
-    res.status(200).send(led[0]);
+    res.status(200).send(led);
 });
 
-app.post('/led', function (req, res) {
+app.post('/color', function (req, res) {
     led[0] = req.body;
     res.send(led[0]);
 });
+
+app.post('/mode', function (req, res) {
+    led[1] = req.body;
+    res.send(led[1]);
+});
+
+app.post('/settings', function (req, res) {
+    led[2] = req.body;
+    res.send(led[2]);
+});
+
 
 
 // Express route for any other unrecognised incoming requests
